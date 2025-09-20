0fc44c7831212b4d1664ce27c165dab6098378cd
patislord <3emirzahitylmz@gmail.com>
2025-09-20 18:18:48 +0300

    moevementpachenson
    malbebecraft

--- Assets/script/movement.cs
+++ Assets/script/movement.cs
@@ -1,47 +1,54 @@
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 
-public class box : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float moveSpeed = 5f;
-    public float zipla = 10f;
-    private Rigidbody2D rb;
-    private bool isGrounded;
-    private SpriteRenderer spriteRenderer;
+    public float jumpForce = 10f;
 
-    [SerializeField] private Animator animator;
+    [Header("Components")]
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private bool isGrounded = false;
+    private bool facingRight = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float move = Input.GetAxis("Horizontal");
+        float move = Input.GetAxisRaw("Horizontal");
 
         // Yatay hareket
         rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
 
-        // Karakter yönü (FlipX)
-        if (move > 0)
-            spriteRenderer.flipX = false;  // sağa bak
-        else if (move < 0)
-            spriteRenderer.flipX = true;   // sola bak
+        // Animasyon koşma kontrolü
+        animator.SetBool("isRunning", move != 0);
+
+        // Karakter yönünü döndürme (scale ile)
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
 
         // Zıplama
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
-            rb.AddForce(Vector2.up * zipla, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
         }
-        if (move != 0)
-        {
-            animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            animator.SetBool("isRunning", false);
-        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1; // X ekseninde ters çevir
+        transform.localScale = scale;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
